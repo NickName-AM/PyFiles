652a960bf6caee286407379da5c4960f68dc71ae
Vin <maharjanebik061@gmail.com>
2022-08-25 10:14:43 +0545

    Optional Extensionw

--- create.py
+++ create.py
@@ -1,13 +1,20 @@
 import os
 import random
+import argparse
 
-ext = 'txt'
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--number", type=int,help="Number of files to create (Default: 10)")
+parser.add_argument("-e", "--extension", type=str, help="Extension of file (Default: txt)")
+args = parser.parse_args()
+
+number = args.number or 10
+ext = args.extension or 'txt'
 
 b = "abcdefghijklmnopqrstuvwxyz"
 c = b.upper() + "0123456789"
 a = b + c
 
-for i in range(20):
+for i in range(number):
     n = random.choice(range(9,12))
     name = "".join(random.choices(a,k=n))
     open(f"{name}.{ext}", 'w').close()
