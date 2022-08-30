18a828a0746d8b5307967509031d22372a343d68
Vin <maharjanebik061@gmail.com>
2022-08-30 17:05:42 +0545

    write data feature

--- create.py
+++ create.py
@@ -1,4 +1,5 @@
 # Create bulk files
+# TO-DO: add optional path feature
 
 '''
 Usage: python3 create.py [options]
@@ -8,7 +9,6 @@ Usage: python3 create.py [options]
 import os
 import random
 import argparse
-import sys
 
 # Possible Patterns
 lowerLetters = "abcdefghijklmnopqrstuvwxyz"
@@ -43,18 +43,31 @@ def generateFilename():
 def create():
     for filename in fileList:
         aFile = filename.strip() + ext
-        open(aFile, "w").close()
+
+        if args.garbage:
+            f = open(aFile, "w")
+            garbageData = "".join(random.choices(allCharacters, k=garbageLength))
+            f.write(garbageData)
+            f.close()
+        else:
+            open(aFile, "w").close()
+        
+        # Verbose info 
         if args.verbose:
             print(f"{aFile} created")
 
 # Command-line arguments
 parser = argparse.ArgumentParser()
+exclude = parser.add_mutually_exclusive_group()
 parser.add_argument("-n", "--number", type=int,help="Number of files to create (Default: 10)")
 parser.add_argument("-e", "--extension", type=str, help="Extension of file (Default: txt)")
-parser.add_argument("-p", "--pattern", type=str, help="Name pattern [Possible values: lower, upper, number, mix] (Defaut: mix)")
 parser.add_argument("-l", "--length", type=int, help="Length of the filename (Default: 11)")
-parser.add_argument("-w", "--wordlist", type=str, help="Get filename with extension from a file. (One name per line)")
+exclude.add_argument("-w", "--wordlist", type=str, help="Get filename with extension from a file. (One name per line)")
+exclude.add_argument("-p", "--pattern", type=str, help="Name pattern [Possible values: lower, upper, number, mix] (Defaut: mix)")
 parser.add_argument("-v", "--verbose", help="Verbose", action="store_true")
+parser.add_argument("-g", "--garbage", help="Write random garbage data", action="store_true")
+parser.add_argument("-gl", "--garbagelength", type=int, help="No. of garbage characters to write (Default: 100)")
+#parser.add_argument("-P", "--path", type=str, help="directory to create files")
 args = parser.parse_args()
 
 # Set the required parameters
@@ -71,6 +84,9 @@ if wordlist and os.path.isfile(wordlist):
     with open(wordlist) as f:
         fileList = f.readlines()[:number]
     ext = ''
+
+if args.garbage:
+    garbageLength = args.garbagelength or 100
 
 # Start creating
 if __name__ == '__main__':
