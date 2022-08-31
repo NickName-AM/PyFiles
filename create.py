62886e9966de14488eab259abe28a13c46ddeac1
Vin <maharjanebik061@gmail.com>
2022-08-31 16:33:24 +0545

    Optional Path feature added

--- create.py
+++ create.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+
 # Create bulk files
-# TO-DO: add optional path feature
 
 '''
 Usage:
@@ -48,48 +48,63 @@ def generateFilename():
 def create():
     for filename in fileList:
         aFile = filename.strip() + ext
+        fileWithPath = os.path.join(dPATH, aFile)
 
+        f = open(fileWithPath, "w")
         if args.garbage:
-            f = open(aFile, "w")
             garbageData = "".join(random.choices(allCharacters, k=garbageLength))
             f.write(garbageData)
-            f.close()
-        else:
-            open(aFile, "w").close()
+        f.close()
         
         # Verbose info 
         if args.verbose:
             print(f"[+] '{aFile}' created")
 
 # Command-line arguments
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="If unexpected values are given, default values will be set.")
 exclude = parser.add_mutually_exclusive_group()
 parser.add_argument("-n", "--number", type=int,help="Number of files to create (Default: 10)")
 parser.add_argument("-e", "--extension", type=str, help="Extension of file (Default: txt)")
 parser.add_argument("-l", "--length", type=int, help="Length of the filename (Default: 11)")
-exclude.add_argument("-w", "--wordlist", type=str, help="Get filename with extension from a file. (One name per line)")
+exclude.add_argument("-w", "--wordlist", type=str, help="Get filename with extension from a file. (One name per line)[Doesn't work with --pattern]")
 exclude.add_argument("-p", "--pattern", type=str, help="Name pattern [Possible values: lower, upper, number, mix] (Defaut: mix)")
-parser.add_argument("-v", "--verbose", help="Verbose", action="store_true")
-parser.add_argument("-g", "--garbage", help="Write random garbage data", action="store_true")
+parser.add_argument("-v", "--verbose", help="Verbose (Default: Off)", action="store_true")
+parser.add_argument("-g", "--garbage", help="Write random garbage data (Default: Off)", action="store_true")
 parser.add_argument("-gl", "--garbagelength", type=int, help="No. of garbage characters to write (Default: 100)")
-#parser.add_argument("-P", "--path", type=str, help="directory to create files")
+parser.add_argument("-P", "--path", type=str, help="directory to create files. (Default: '.' current directory)")
 args = parser.parse_args()
 
+
 # Set the required parameters
+# Number of files to create
 number = abs(args.number or 10)
+
+# Extension of the files
 ext = "." + (args.extension or "txt")
+
+# Charset to use
 charset = determineCharset(args.pattern)
+
+# length of filename (without ext)
 namesize = abs(args.length or 11)
+
+# wordlist with filenames
 wordlist = args.wordlist
+
+# Directory to create files in
+if args.path:
+    # If the directory exists choose that path, else choose the current working directory
+    dPATH = os.path.isdir(args.path) and args.path or os.getcwd()   
 
 fileList = generateFilename()
 
-# If wordlist is given, change fileList
+# If wordlist is given, use that wordlist instead of default generator
 if wordlist and os.path.isfile(wordlist):
     with open(wordlist) as f:
         fileList = f.readlines()[:number]
     ext = ''
 
+# length of garbage data to write
 if args.garbage:
     garbageLength = abs(args.garbagelength or 100)
 
