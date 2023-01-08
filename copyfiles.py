f6604d42af35c96e83865bb47e8d2bc126c4e897
Vin <maharjanebik061@gmail.com>
2023-01-08 19:13:24 +0545

    changed 'extension' to positional argument and added endswith option

--- copyfiles.py
+++ copyfiles.py
@@ -14,8 +14,9 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument('startpath', help='Start searching from the given path')
 parser.add_argument('copyto', type=str, help='Copy the matching files to the given path')
-parser.add_argument('--ext', type=list, help='extensions(s) to search for')
+parser.add_argument('ext', type=list, help='extensions(s) to search for')
 parser.add_argument('--startswith', type=str, help='Filename starting with the given string and ending with given extension')
+parser.add_argument('--endswith', type=str, help='Filename ending with the given string and extension')
 args = parser.parse_args()
 
 # Path to start the search from
@@ -30,6 +31,9 @@ ext_list = ''.join(args.ext).split(',')
 # Startswtih
 starts_with = args.startswith or ''
 
+# Endswith
+ends_with = args.endswith or ''
+
 # This might be the only place where I have used De-Morgan's Law
 if not (os.path.exists(start_dir) and os.path.exists(dst_dir)):
     print('STARTPATH_OR_DESTINATIONPATH_DOES_NOT_EXIST')
@@ -39,9 +43,11 @@ if not (os.path.exists(start_dir) and os.path.exists(dst_dir)):
 for root, dirs, files in os.walk(args.startpath):
 
     # Gather file(s) with the given ext(s) from the current dir
-    files_to_copy = [os.path.join(root, file) for file in files for e in ext_list if file.endswith(e) and file.startswith(starts_with)]
+    files_to_copy = [os.path.join(root, file) for file in files for e in ext_list if file.startswith(starts_with) and file.endswith(f'{ends_with}.{e}')]
     
+    print(files_to_copy)
+
     # If there are files with given ext in that dir, copy them to dst_dir
-    if files_to_copy:
-        for f in files_to_copy:
-            shutil.copy2(f, dst_dir)
+    # if files_to_copy:
+        # for f in files_to_copy:
+            # shutil.copy2(f, dst_dir)
