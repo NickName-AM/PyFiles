d1d546a2d7873b3e8a69cd838a815e9fdfa4a75c
Vin <maharjanebik061@gmail.com>
2023-01-28 21:57:23 +0545

    adds file(s) and dir(s) exclusion

--- copyfiles.py
+++ copyfiles.py
@@ -2,8 +2,7 @@
 
 # Copy a bunch of files
 
-# Looks for files with matching  pattern starting from the given directory
-# until there are no directories left to traverse through
+# Looks for files with matching pattern in the given directory (or recursively)
 
 '''
 python3 copyfiles.py --help
@@ -20,6 +19,8 @@ parser.add_argument('copyto', type=str, help='Copy the matching files to the giv
 parser.add_argument('--ext', type=list, help='extensions(s) to search for')
 parser.add_argument('--startswith', type=str, help='Filename starting with the given string and ending with given extension')
 parser.add_argument('--endswith', type=str, help='Filename ending with the given string and extension')
+parser.add_argument('--exclude-files', type=str, help='List of filenames to exclude (seperated by comma)')
+parser.add_argument('--exclude-dirs', type=str, help='List of directories to exclude (seperated by comma)')
 parser.add_argument('-r', '--recursive', help='Copy files recursively. (Default: Off)', action='store_true')
 args = parser.parse_args()
 
@@ -44,13 +45,25 @@ starts_with = args.startswith or ''
 # Endswith
 ends_with = args.endswith or ''
 
+exclude_files = []
+if args.exclude_files:
+    exclude_files = args.exclude_files.split(',')
+
+exclude_dirs = []
+if args.exclude_dirs:
+    exclude_dirs = args.exclude_dirs.split(',')
+
 # This might be the only place where I have used De-Morgan's Law
 if not (os.path.exists(start_dir) and os.path.exists(dst_dir)):
-    print('STARTPATH_OR_DESTINATIONPATH_DOES_NOT_EXIST')
+    print('[-] STARTPATH_OR_DESTINATIONPATH_DOES_NOT_EXIST')
     exit(-1)
 
 # Main
 for root, dirs, files in os.walk(args.startpath):
+
+    excluded_dir = [i for i in exclude_dirs if i in root]
+    if excluded_dir:
+        continue    
 
     # Gather file(s) with the given ext(s) from the current dir
     ## I know the below code is messy, but I was practicing list comprehension
@@ -59,6 +72,9 @@ for root, dirs, files in os.walk(args.startpath):
     # If there are files with given ext in that dir, copy them to dst_dir
     if files_to_copy:
         for f in files_to_copy:
+            if f.split('/')[-1] in exclude_files:
+                continue
+            
             shutil.copy2(f, dst_dir)
     
     if not args.recursive:
